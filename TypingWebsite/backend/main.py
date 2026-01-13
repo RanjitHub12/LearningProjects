032562dac39f0db90f3808f21f9f416f4d055e78
RANJIT SINGH <ranjits121212@gmail.com>
2026-01-14 00:25:14 +0530

    add cors error

--- TypingWebsite/backend/main.py
+++ TypingWebsite/backend/main.py
@@ -34,7 +34,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://learning-projects-woad.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
