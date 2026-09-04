68e0a0df1f611afce5138d8287f6f7515d933ce8
RANJIT SINGH <ranjits121212@gmail.com>
2026-09-04 21:00:14 +0530

    chore: push pending changes for deployment

--- CodeVault/powershell.bat
+++ CodeVault/powershell.bat
@@ -1,5 +1,7 @@
 @echo off
+git status
 git add -A
-git commit -m "feat: implement per-user data isolation and scoped database wipes"
+git commit -m "chore: push pending changes for deployment"
 git push origin master
-echo "Successfully pushed to git!"
+echo "Finished git push check!"
+del "%~f0"
